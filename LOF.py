ad7bd5329831505820ca595e944d6eb0750fea57
audrms99 <arm06201@gmail.com>
2024-02-19 22:57:52 +0900

    기능만 구현

--- LOF.py
+++ LOF.py
@@ -46,22 +46,7 @@ data = torch.randn(num_samples, num_features)
 distances = torch.cdist(data, data)
 
 # k 값 선택 (가장 가까운 이웃의 수)
-k = 5
+# k = 5
 
 # LOF 점수 계산
-lof_scores = lof_instance.local_outlier_factor(k, data, distances)
-
-# 원점으로부터 각 데이터 포인트의 거리 계산
-distances_from_origin = torch.norm(data, dim=1)
-
-# +-1 범위 내에 있는 데이터 포인트를 파란색으로, 나머지를 빨간색으로 설정
-colors = ['blue' if torch.abs(distance) <= 1 else 'red' for distance in distances_from_origin]
-
-# 데이터 시각화
-plt.figure(figsize=(10, 6))
-plt.scatter(data[:, 0], data[:, 1], c=colors)
-#plt.colorbar(label='Determination of defective hydrogen')
-plt.title('Data visualization according to distance from origin')
-plt.xlabel('temperature and humidity')
-plt.ylabel('press')
-plt.show()
+# lof_scores = lof_instance.local_outlier_factor(k, data, distances)

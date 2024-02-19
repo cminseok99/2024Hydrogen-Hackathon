13e1db00d4dd2945aaa221335e482473bef371d4
audrms99 <arm06201@gmail.com>
2024-02-19 22:58:34 +0900

    main

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.neighbors import LocalOutlierFactor
-from matplotlib.pyplot import figure
-figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+import pandas as pd
+
+#LOF.py에서 class LOF 호출
+from LOF import LOF
+
+file_path = 'random_data.csv'
+data = pd.read_csv(file_path)
+
+# 원점으로부터 각 데이터 포인트의 거리 계산
+distances_from_origin = torch.norm(data, dim=1)
 
 
-np.random.seed(42)
 
-# Generate train data
-X_inliers = 0.3 * np.random.randn(100, 2)#정규분포에서 100*2만들고
-X_inliers = np.r_[X_inliers + 2, 2*X_inliers - 2]#각각 2,2 혹은 -2,-2만큼 평행이동한거를 vstack. 즉 cluster 2개
+# +-1 범위 내에 있는 데이터 포인트를 파란색으로, 나머지를 빨간색으로 설정
+colors = ['blue' if torch.abs(distance) <= 1 else 'red' for distance in distances_from_origin]
 
-# Generate some outliers
-X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
-X = np.r_[X_inliers, X_outliers]#-4,4에서 뽑은 outlier와 inlier를 vstack
-
-n_outliers = len(X_outliers)
-ground_truth = np.ones(len(X), dtype=int)
-ground_truth[-n_outliers:] = -1
-
-# fit the model for outlier detection (default)
-clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
-# use fit_predict to compute the predicted labels of the training samples
-# (when LOF is used for outlier detection, the estimator has no predict,
-# decision_function and score_samples methods).
-y_pred = clf.fit_predict(X) #1,-1로 나온다.
-n_errors = (y_pred != ground_truth).sum()
-X_scores = clf.negative_outlier_factor_
-
-#fig, ax = plt.subplots()
-plt.title("Local Outlier Factor (LOF)")
-plt.scatter(X[:, 0], X[:, 1], color='b', s=3., label='Data points')
-# plot circles with radius proportional to the outlier scores
-radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min()) #오홍 minmax scaling으로 radius를 정햇네
-plt.scatter(X[:, 0], X[:, 1], s=1000 * radius, edgecolors='r',
-            facecolors='none', label='Outlier scores')
-n=np.copy(X_scores)
-n[n>-1.3]=np.nan
-n=np.round(n,2)
-for i, txt in enumerate(n):
-    if np.isnan(txt):continue
-    plt.annotate(txt, (X[i,0], X[i,1]))
-legend = plt.legend(loc='upper left')
+# 데이터 시각화
+plt.figure(figsize=(10, 6))
+plt.scatter(data[:, 0], data[:, 1], c=colors)
+plt.title('Data visualization according to distance from origin')
+plt.xlabel('temperature and humidity')
+plt.ylabel('press')
 plt.show()
